a7b866af53be2c6120a6af2288becfcf2877568b
Stepan Lusnikov <endenwer@gmail.com>
2019-04-02 15:05:31 +0800

    Add areas and bike models

--- src/bali_bike_web/constants.cljs
+++ src/bali_bike_web/constants.cljs
@@ -1,10 +1,24 @@
 (ns bali-bike-web.constants)
 
 (def models
-  {1 "Honda Vario"
-   2 "Honda Scoopy"})
+  {1 "Honda Scoopy"
+   2 "Honda Vario 125cc"
+   3 "Honda Vario 150cc"
+   4 "Yamaha NMax"
+   5 "Yamaha XMax"})
 
 (def areas
-  {1 "Canggu"
-   2 "Kuta"
-   3 "Kerobokan"})
+  {1 "Benoa"
+   2 "Canggu"
+   3 "Denpasar"
+   4 "Jimbaran"
+   5 "Kerobokan"
+   6 "Kuta"
+   7 "Legian"
+   8 "Lembongan"
+   9 "Nusa Dua"
+   10 "Sanur"
+   11 "Seminyak"
+   12 "Ubud"
+   13 "Uluwatu"
+   14 "Umalas"})
