9c8485d1ae8deb9ffa0c7c21999e8b6e996c6ad8
Stepan Lusnikov <endenwer@gmail.com>
2019-07-15 06:53:57 +0300

    Change map when use autocomplete

--- src/bali_bike_web/ui/form_modal.cljs
+++ src/bali_bike_web/ui/form_modal.cljs
@@ -117,18 +117,29 @@
                       :on-change on-change}]])
 
 (defn render-address
-  []
-  [ant/form-item {:label "Address"
-                  :label-col {:xs {:span 24} :sm {:span 6}}
-                  :wrapper-col {:xs {:span 24} :sm {:span 18}}
-                  :className "address-input"}
-   [geosuggestion {:input-class-name "ant-input"
-                   :suggests-class-name "ant-select-dropdown-menu ant-select-dropdown-menu-root"
-                   :suggest-item-class-name "ant-select-dropdown-menu-item"}]
-   [google-map {:id "google-map"
-                :mapContainerStyle {:width "100%" :height "400px"}
-                :zoom 8
-                :center {:lat -8.745308699651275 :lng 115.16695126891136}}]])
+  [{:keys [value on-change]}]
+  (r/with-let [map-instance (r/atom nil)]
+    (let [{:keys [lat lng address]} value]
+      [ant/form-item {:label "Address"
+                      :label-col {:xs {:span 24} :sm {:span 6}}
+                      :wrapper-col {:xs {:span 24} :sm {:span 18}}
+                      :className "address-input"}
+       [geosuggestion {:input-class-name "ant-input"
+                       :suggests-class-name "ant-select-dropdown-menu ant-select-dropdown-menu-root"
+                       :suggest-item-class-name "ant-select-dropdown-menu-item"
+                       :contry "ID"
+                       :on-suggest-select #(on-change (if %
+                                                        {:address (.-label %)
+                                                         :lat (.-location.lat %)
+                                                         :lgn (.-location.lng %)}
+                                                        {}))}]
+       [google-map {:id "google-map"
+                    :mapContainerStyle {:width "100%" :height "400px"}
+                    :zoom (if address 15 8)
+                    :center {:lat (or lat -8.745308699651275)
+                             :lng (or lng 115.16695126891136)}
+                    :on-load #(reset! map-instance %)
+                    :on-drag-end #(.log js/console %)}]])))
 
 (defn render-bike-photo-item
   [{:keys [url status progress removePhoto] :as params}]
@@ -218,7 +229,8 @@
                             :is-valid? @(f/is-valid-path? form :photos-count)
                             :photos photos}]
        [ant/divider {:orientation "left"} "Address"]
-       [render-address]
+       [render-address {:value (:address @form-data)
+                        :on-change #(on-change :address %)}]
        [render-bike-areas {:area-ids (:area-ids @form-data)
                            :is-valid? @(f/is-valid-path? form :area-ids)
                            :on-change #(on-change :area-ids (js->clj %))}]
